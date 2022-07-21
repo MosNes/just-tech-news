0c2da1b1f3ae2ac47ddb4c5b35de8283f14c1edf
MosNes <7hekarl@gmail.com>
2022-07-21 16:16:49 -0400

    created Post.js and started creating Post model

---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create post model
+class Post extends Model {}
+
+// create fields and columns for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+    }
+)
