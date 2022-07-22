e5574ea41b8829028adbe1f77aa4e6e00b3825c2
MosNes <7hekarl@gmail.com>
2022-07-22 15:11:23 -0400

    added post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 // use userRoutes endpoints for all /users/ requests
 router.use('/users', userRoutes);
+
+//use postRoutes for all /posts/ requests
+router.use('/posts', postRoutes);
 
 module.exports = router;
