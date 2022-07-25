9e314df15e72f7f3ff5b1782e23b4b778dc92024
MosNes <7hekarl@gmail.com>
2022-07-25 16:11:15 -0400

    added style.css and updated server.js to server static files

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 require('dotenv').config();
 
 //---------------------DEPENDENCIES AND GLOBAL VARIABLES--------------------------------------
-
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//required for users to see static assets in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use(routes);
