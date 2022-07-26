d5b6ff8b61c6a500f5720c78326feaa128a67444
MosNes <7hekarl@gmail.com>
2022-07-26 14:38:45 -0400

    created front end logic for login

---
+++ public/javascript/login.js
@@ -0,0 +1,66 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    //get input from form and trim whitespace
+    const username = document.getElementById('username-signup').value.trim();
+    const email = document.getElementById('email-signup').value.trim();
+    const password = document.getElementById('password-signup').value.trim();
+
+    //validate input
+    if(username && email && password) {
+        //make fetch call to api route
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+        
+        //check response data
+        if (response.ok) {
+            console.log('success');
+        } else {
+            //throw error alert
+            alert(response.statusText);
+        }
+
+    }
+}
+
+async function loginFormHandler(event) { 
+    event.preventDefault();
+
+    //get input from form and trim whitespace
+    const email = document.getElementById('email-login').value.trim();
+    const password = document.getElementById('password-login').value.trim();
+
+    //validate input
+    if(email && password) {
+        //make fetch call to api route
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+        
+        //check response data
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            //throw error alert
+            alert(response.statusText);
+        }
+    }
+}
+
+//test user credentials
+//boogie@js.com
+//wopwop44
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
