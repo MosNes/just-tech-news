b6efc404b98f8b45fd378ab4d89af86fa1ad7a2c
MosNes <7hekarl@gmail.com>
2022-07-29 16:12:09 -0400

    added format_url function to helpers

--- __tests__/helper.test.js
+++ __tests__/helper.test.js
@@ -1,4 +1,4 @@
-const {format_date, format_plural} = require('../utils/helpers');
+const {format_date, format_plural, format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2022-07-29 16:12:03');
@@ -16,3 +16,13 @@ test('format_plural() returns a singular word', () => {
     
     expect(format_plural('Lion', 1)).toEqual('Lion');
 });
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+  });
--- utils/helpers.js
+++ utils/helpers.js
@@ -10,5 +10,17 @@ module.exports = {
             return word;
         }
         return `${word}s`;
+    },
+
+    format_url: url => {
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            //after the initial http:// has been remove, cut off everything
+            //after a '/'
+            .split('/')[0]
+            //cut off everything after a '?'
+            .split('?')[0]
     }
 }
